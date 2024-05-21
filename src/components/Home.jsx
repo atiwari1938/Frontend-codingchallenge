4287c79dcbe66f4993db2c4b107ff9678c280179
Ankit Tiwari <atiwari1938@gmail.com>
2024-05-21 14:45:06 +0530

    home page modified

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import banner1 from '../assets/image2.png';
+import banner2 from '../assets/image1.jpg';
+import banner3 from '../assets/image 3.jpg';
 
 const Home = () => {
   return (
     <div>
-      <h1>Welcome to Doctor Management System</h1>
-      <p>This is a system to manage doctors' information.</p>
+      <h1 style={{textAlign:'center',color:'lightblue'}}>Welcome to Doctor Management System</h1>
+      <p style={{textAlign:"center"}}>This is a system to manage doctors' information.</p>
+      <Carousel autoPlay interval={3000} infiniteLoop showThumbs={false} showStatus={false}>
+        <div>
+          <img src={banner1} alt="Banner 1" />
+          <p className="legend">Legend 1</p>
+        </div>
+        <div>
+          <img src={banner2} alt="Banner 2" />
+          <p className="legend">Legend 2</p>
+        </div>
+        <div>
+          <img src={banner3} alt="Banner 3" />
+          <p className="legend">Legend 3</p>
+        </div>
+      </Carousel>
     </div>
   );
 }
